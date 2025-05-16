5cf70005a886a954ecd2bb6981fd387d235088df
Daniel Alamilla <daniel.alamilla.co@gmail.com>
2025-05-16 12:18:44 -0600

    update euler project

---
+++ euler/tests/euler-004.test.js
@@ -0,0 +1,11 @@
+const Euler004 = require("../src/euler-004");
+
+describe("the largest palindrome made from the product of two n-digit numbers", () => {
+  test.each`
+    input | expectedResult
+    ${2}  | ${9009}
+    ${3}  | ${906609}
+  `("$input should return $expectedResult", ({ input, expectedResult }) => {
+    expect(Euler004(input)).toBe(expectedResult);
+  });
+});
